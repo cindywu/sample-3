7bf9e4a07cc3ede47ca74d7c65ebda9c7537d4e1
cindy <1177031+cindywu@users.noreply.github.com>
2023-05-14 18:03:22 -1000

    checkpoint 6:03pm 57 min - add a neighborhood

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,23 @@
 import HelloWorld from '../components/helloWorld'
+import { useState, useEffect, useRef } from 'react'
 
 import { supabase } from '../lib/supabaseClient'
 export default function Home({ neighborhoods }) {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  async function addNewHood() {
+    try {
+      // create new neighborhood in db
+      const { error } = await supabase
+        .from('neighborhoods')
+        .insert({ name: inputRef.current.value }) // can we get away without including an id
+    } catch (error) {
+      console.log({ error })
+    } finally {
+      alert('thing did a thing!')
+    }
+  }
+
   return (
     <div>
       <HelloWorld />
@@ -10,6 +26,16 @@ export default function Home({ neighborhoods }) {
           <li key={hood.id}>{hood.name}</li>
         ))}
       </ul>
+      <div className={'p-4 flex flex-col w-96 bg-orange-200'}>
+        <div>new hood</div>
+        <input placeholder={'type new hood'} ref={inputRef}></input>{' '}
+        <button
+          className={'bg-orange-500 my-4 hover:bg-orange-600'}
+          onClick={() => addNewHood()}
+        >
+          add hood
+        </button>
+      </div>
     </div>
   )
 }
